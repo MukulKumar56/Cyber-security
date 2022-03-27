1a2c9a7a729645a6a0ea59db3e60546669c2417f
Mukulkumar56 <Mukulkumareh88@gmail.com>
2022-03-26 23:15:41 -0700

    first commit

---
+++ assets/js/navbar.js
@@ -0,0 +1,9 @@
+function control(){
+    let nav = document.querySelector('.mobile');
+    if (nav.style.display==='block'){
+        nav.style.display='none';
+    }else{
+        nav.style.display='block';
+
+    }
+}
